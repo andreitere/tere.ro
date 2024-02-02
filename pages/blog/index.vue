ef558276c48bbf17abf0705be9a1c90ea61a61cf
Andrei Terecoasa <terecoasa.andrei@gmail.com>
2024-02-02 16:03:26 +0200

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -9,7 +9,7 @@ const data = await queryContent('/blog').where({draft: false}).sort({date: -1}).
   <div class="mx-auto max-w-2xl space-y-6">
     <div>
       <Menu/>
-      <h1 class="text-2xl font-bold">Blog</h1>
+      <h1 class="text-2xl font-bold">Tips & Tricks</h1>
     </div>
     <div v-for="article in data" :key="article._path" class="flex flex-col space-y-2 rounded p-3 border-[1px] border-solid border-gray-100 hover:border-gray-300 transition dark:border-gray-800">
       <NuxtLink :to="article._path"><h2 class="text-lg font-semibold leading-normal text-pretty">{{ article.title }}</h2></NuxtLink>
