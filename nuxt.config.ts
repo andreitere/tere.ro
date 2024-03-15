59d1f0284436f5e3d610be13936b8a2843ff06d9
Andrei Terecoasa <terecoasa.andrei@gmail.com>
2024-03-15 16:00:26 +0200

    add umami

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {enabled: true},
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/content', '@nuxtjs/google-fonts', 'unplugin-icons/nuxt', 'nuxt-og-image', 'dayjs-nuxt', 'nuxt-gtag'],
-  css: ['~/assets/css/main.scss'],
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxt/content",
+    "@nuxtjs/google-fonts",
+    "unplugin-icons/nuxt",
+    "nuxt-og-image",
+    "dayjs-nuxt",
+    "nuxt-gtag",
+    "nuxt-umami",
+  ],
+  css: ["~/assets/css/main.scss"],
   dayjs: {
-    plugins: ['calendar'],
-    defaultLocale: 'en',
+    plugins: ["calendar"],
+    defaultLocale: "en",
   },
   postcss: {
     plugins: {
@@ -14,38 +24,44 @@ export default defineNuxtConfig({
     },
   },
   site: {
-    url: "https://tere.ro"
+    url: "https://tere.ro",
   },
   ogImage: {
     fonts: [
       // will load the Noto Sans font from Google fonts
-      'Fira+Code:600',
-      'Fira+Code:700'
-    ]
+      "Fira+Code:600",
+      "Fira+Code:700",
+    ],
   },
   content: {
     highlight: {
       theme: {
         // Default theme (same as single string)
-        default: 'github-dark',
-      }
+        default: "github-dark",
+      },
     },
   },
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: '',
+    prefix: "",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
+    componentDir: "./components/ui",
   },
   googleFonts: {
-    families: ['Fira Code', "Roboto Slab", "Space Mono"]
+    families: ["Fira Code", "Roboto Slab", "Space Mono"],
   },
   gtag: {
-    id: 'G-KBKD6E08Y4'
-  }
-})
+    id: "G-KBKD6E08Y4",
+  },
+  appConfig: {
+    umami: {
+      host: "https://analytics.cloudcrafts.club",
+      id: "b9f4ffc4-3bcb-44b5-a2f6-9672d68a4761",
+    },
+  },
+});
