99fe0b78a510e45eb02b2637226565629d9f7a2d
Andrei Terecoasa <terecoasa.andrei@gmail.com>
2025-03-19 08:26:33 +0200

    f

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2025-03-19",
   devtools: { enabled: true },
   ssr: true,
   modules: [
